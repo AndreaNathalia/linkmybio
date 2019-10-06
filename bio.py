7d0985f1a4bd7293bf7f7e2ed6db1e8537140208
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2019-10-06 00:50:42 -0600

    modify the info yaml, read the yaml, fill the files and add style

---
+++ bio.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import yaml
+
+bio = Flask(__name__)
+
+with open("links.yaml") as yml:
+    links_yaml = yaml.load(yml, Loader = yaml.FullLoader)
+
+## variables con info del yaml
+title = links_yaml["title"]
+pic = links_yaml["picture"]
+name = links_yaml["name"]
+short_bio = links_yaml["shortbio"]
+
+# Links
+fb_link = links_yaml["links"]["facebook"]["link"]
+ig_link = links_yaml["links"]["instagram"]["link"]
+pin_link = links_yaml["links"]["pinterest"]["link"]
+blog_link = links_yaml["links"]["blog"]["link"]
+
+# Descriptions
+fb_description = links_yaml["links"]["facebook"]["description"]
+ig_description = links_yaml["links"]["instagram"]["description"]
+pin_description = links_yaml["links"]["pinterest"]["description"]
+blog_description = links_yaml["links"]["blog"]["description"]
+
+
+@bio.route("/")
+def linktree():
+    return render_template('linktree.html', 
+    links_yaml = links_yaml, title = title, 
+    pic = pic, name = name, short_bio = short_bio, 
+    fb_link = fb_link, ig_link = ig_link, pin_link = pin_link, blog_link = blog_link,
+    fb_description = fb_description, ig_description = ig_description, pin_description = pin_description, blog_description = blog_description)
+
+bio.run(host="0.0.0.0", debug=True)
